901d7c1816e13608cc2482186ab32fc7f88b317e
lzh <pleasexiaolin@163.com>
2025-11-21 21:30:35 +0800

    info:项目初始化 user:小林 date:2025/11/21

---
+++ src/main/java/com/xiaolin/ymall/YmallApplication.java
@@ -0,0 +1,13 @@
+package com.xiaolin.ymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaolin/ymall/YmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaolin.ymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
